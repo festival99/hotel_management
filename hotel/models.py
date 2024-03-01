975054e49452ea5a80a501c8d2334742968770bb
festival99 <utsavbhowmikbd00@gmail.com>
2024-03-01 10:35:22 +0600

    booking base done

--- hotel/models.py
+++ hotel/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings #user model from settings
 # Create your models here.
 class Room(models.Model):
     Room_Categories=(
@@ -17,3 +17,16 @@ class Room(models.Model):
 
     def __str__(self): #whenever you do something with your model it will show you this representation
         return f'{self.number}.{self.category} with {self.beds} beds for {self.capacity} people'
+
+
+class Booking(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) #settings have authUsermodel,
+    room=models.ForeignKey(Room, on_delete=models.CASCADE)
+    check_in=models.DateTimeField()
+    check_out=models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.user} has booked {self.room} from {self.check_in} to {self.check_out}'
+    
+
+
