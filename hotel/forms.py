c3af756b451b4c9714bfd40e17b0621112c34774
festival99 <utsavbhowmikbd00@gmail.com>
2024-03-23 05:58:47 +0600

    user booking panel

--- hotel/forms.py
+++ hotel/forms.py
@@ -3,15 +3,6 @@ from django import forms
 #if  room is available book straight away,otherwise redirect to other section
 
 class AvailabilityForm(forms.Form):
-    #we need room category,check in and check out
-    Room_Categories=(
-        ('YAC', 'AC'),
-        ('NAC', 'NON-AC'),
-        ('DEL', 'DELUXE'),
-        ('KIN', 'KING'),
-        ('QUE', 'QUEEN'),
-    )
     
-    room_category=forms.ChoiceField(choices=Room_Categories, required=True)
     check_in=forms.DateTimeField(required=True, input_formats=["%Y-%m-%dT%H:%M" ,])
     check_out=forms.DateTimeField(required=True, input_formats=["%Y-%m-%dT%H:%M" ,])
--- hotel/urls.py
+++ hotel/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import RoomList, BookingList,BookingView
+from .views import RoomListView, BookingList,BookingView, RoomDetailView
 
 app_name='hotel'
 
 urlpatterns=[
-    path('room_list/',RoomList.as_view(), name='RoomList'), #view er Roomlist ke room_list hishebe view korbe
+    path('room_list/',RoomListView, name='RoomList'), #view er Roomlist ke room_list hishebe view korbe
     path('booking_list/',BookingList.as_view(), name='BookingList'), # view er BookingList ke booking_list hishebe url nibe 
-    path('book/',BookingView.as_view(), name='booking_view'),
+    path('book/',BookingView.as_view(), name='Bookingview'),
+    path('room/<category>', RoomDetailView.as_view(), name='RoomDetailView'),
 ]
 
 
